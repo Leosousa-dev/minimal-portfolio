befdc3f56568d2ce779a243cad7c7bab361c2b7e
leonardo sousa <leonardocasa790@gmail.com>
2023-03-14 22:23:31 -0300

    create hero

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const menu = document.getElementById('btn-mobile');
-
-function Tooglemenu(){
-   const nav = document.querySelector('nav');
-   nav.classList.toggle('activeMenu');
-}
-
-menu.addEventListener('click', Tooglemenu);
